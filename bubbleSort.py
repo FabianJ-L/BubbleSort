29dfc7619c3d9737318f3239b0333b8dcc9e8847
FabianJ-L <fabian.janisch-lang@sr.htlweiz.at>
2025-12-04 16:29:44 +0100

    finished BubbleSort

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+def bubble_sort(numberList):
+    n = len(numberList)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numberList[j] > numberList[j + 1]:
+                numberList[j], numberList[j + 1] = numberList[j + 1], numberList[j]
+    return numberList
+
+def main():
+    print("BubbleSort")
+    arr = [1, 0, 7, 1, 6, 2, 9, 14, 2]
+    print(bubble_sort(arr))
+
+main()
